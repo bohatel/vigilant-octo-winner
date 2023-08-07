aec62951ab4b0aa96e6add13d18ee80935a2fcd9
bohatel <razvan.bohatel@gmail.com>
2023-08-07 13:59:35 +0300

    refactor app endpoints to lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+use zero2prod::run;
+
+// async fn greet(req: HttpRequest) -> impl Responder {
+//     let name = req.match_info().get("name").unwrap_or("World");
+//     format!("Hello {}!", name)
+// }
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let listener = TcpListener::bind("127.0.0.1:8000")
+        .expect("Failed to bind address!");
+    run(listener)?.await
 }
